23793effe69ad32c18a0dc1d533c669a2d1a4a36
Alex <koshka3564@gmail.com>
2024-10-19 14:36:46 +0300

    security and cron-enable

--- src/main/java/com/example/springbootsp/SpringBootApplication.java
+++ src/main/java/com/example/springbootsp/SpringBootApplication.java
@@ -1,12 +1,39 @@
 package com.example.springbootsp;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractAuthenticationFilterConfigurer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
+@EnableScheduling
+@EnableWebSecurity
 @org.springframework.boot.autoconfigure.SpringBootApplication
 public class SpringBootApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootApplication.class, args);
     }
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService() {
+        UserDetails user = User.withDefaultPasswordEncoder().username("root").password("root") // change before production
+                .roles("USER").build();
+        return new InMemoryUserDetailsManager(user);
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable).authorizeRequests().requestMatchers("/login", "/css/**", "/js/**").permitAll().anyRequest().authenticated().and().formLogin(AbstractAuthenticationFilterConfigurer::permitAll).logout(LogoutConfigurer::permitAll);
+        return http.build();
+    }
+
 
 }
